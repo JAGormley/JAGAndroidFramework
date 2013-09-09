237e39f9c410129cb90e81b4a9a588c23a60c4fd
Andrew Gormley <andrewjgormley@gmail.com>
2013-09-08 18:14:04 -0700

    next

--- src/com/jag/positron/Tutorial.java
+++ src/com/jag/positron/Tutorial.java
@@ -1,8 +1,14 @@
 package com.jag.positron;
 
+import com.jag.framework.Game;
 import com.jag.framework.Screen;
 
 public class Tutorial extends Screen {
+
+	public Tutorial(Game game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public void update(float deltaTime) {
